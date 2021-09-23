30cd73b54933302dd977393449f477e81702b0ae
swynnejr <saucersam@gmail.com>
2021-09-22 22:36:09 -0500

    Implemented help function

--- calculator.py
+++ calculator.py
@@ -1,9 +1,7 @@
-
-
 class Calculator:
 
     def calculate(self, expression):
-        stack = []
+        global stack
         expressionList = expression.split(" ")
         try:
             for element in expressionList:
@@ -26,11 +24,14 @@ class Calculator:
                     else:
                         number = float(element)
                     stack.append(number)
-                # print(stack)
-            return stack.pop()
+                print(stack)
+            return stack[-1]
         except:
+            print("")
             print("I wasn't able to process your request:")
-            print(user_input)
+            print("")
+            print("Invalid Input = " + user_input)
+            print("")
             print("Please only input numbers and + - * / operators separated by a space.")
 
 print("")
@@ -41,12 +42,63 @@ print("")
 print("  < This version currently handles + - * / operators >")
 print("")
 calc = Calculator()
-user_input = input("> ")
-if user_input.lower() not in ["exit", "quit", "stop", "q", "close"]:
+user_input = ""
+stack = []
+while user_input.lower() not in ["exit", "quit", "stop", "q", "close", "help", "h"]:
+    user_input = input("> ")
     output = calc.calculate(user_input)
     print(output)
+if user_input.lower() in ["help", "h"]:
+    print("  ◦◦◦ HELP ◦◦◦ ")
+    print("")
+    print("  ◦ Reverse Polish Notation is also known as 'postfix' notation ")
+    print("")
+    print("  ◦ This means your operator symbols + - * / are written AFTER your numerical operands ")
+    print("")
+    print("  ◦ For example: '2 2 +' will result in '4'")
+    print("")
+    print("  ◦ This also means '2 + 2' is an invalid input")
+    print("")
+    print("      < ANOTHER EXAMPLE >")
+    print("")
+    print("  ◦ '2 2 2 + +' will result in '6' ")
+    print("")
+    print("  ◦ In the event of - or /, an RPN calculator will 2nd to last entry and subtract of divide by the last entry ")
+    print("")
+    print("  ◦ After each operator symbol is executed, the resulting value is put back in the list")
+    print("")
+    print("      < MORE COMPLICATED EXAMPLE >")
+    print("  10 4 3 2 1 + + + /")
+    print("")
+    print("      < PROCESS >")
+    print("  [10]")
+    print("  [10, 4]")
+    print("  [10, 4, 3]")
+    print("  [10, 4, 3, 2]")
+    print("  [10, 4, 3, 2, 1]")
+    print("  [10, 4, 3, 3]")
+    print("  [10, 4, 6]")
+    print("  [10, 10]")
+    print("  [1.0]")
+    print("")
+    print("      < RESULT >")
+    print("  1.0 ")
+    
+
+
+
+
+
+
+
+
+
+
 else:
     print("")
     print("  < Thank you, have a nice day >")
     print("")
     print("  < Exiting the calculator >")
+
+
+
